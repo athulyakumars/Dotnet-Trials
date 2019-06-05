6491871667795ceac03182fbbef619565559dbb6
athulyakumars <athulya.s@experionglobal.local>
2019-06-05 17:11:05 +0530

    Initial Commit

---
+++ IdentityImplemetation/IdentityImplemetation/src/IdentityImplemetation/Models/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace IdentityImplemetation.Models
+{
+    // For implemeting the Asp .net Identity, use IdentityDbContext;
+    //otherwise can be use DbContext
+    public class AppDbContext : IdentityDbContext 
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            :base(options)
+        {
+
+        }
+    }
+}
---
+++ Swagger/Swagger/src/Swagger/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Swagger.Controllers
+{
+    [Route("api/[controller]")]
+    public class AccountController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> GetAccountNames()
+        {
+            return new string[] { "Salesboost", "Benchmark", "BestWestern","Clubcorp" };
+        }
+    }
+}
